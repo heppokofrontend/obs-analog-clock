09be019e7005756d633a784cfefce47ed225ac17
heppokofrontend <heppokofrontend@gmail.com>
2021-12-06 00:09:06 +0900

    :wrench: Update

--- clean.js
+++ clean.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const dirname = 'dist';
+const {version} = require('./package.json');
 
 try {
-  fs.rmdirSync('./dist', {
+  fs.rmdirSync(`obs-analog-clock-v${version}`, {
     recursive: true,
   });
 } catch {}
